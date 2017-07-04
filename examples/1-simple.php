cbfcbe5729428253b2098545045d33725e5be662
Vinh Duong <ngoc.lo.dang@gmail.com>
2017-07-04 11:45:50 +0700

    add example

--- examples/1-simple.php
+++ examples/1-simple.php
@@ -2,12 +2,38 @@
 include '../db.php';
 
 var_dump($db->query('select 1'));
-
 /*
 This will output:
 
-array(1){
-	1 => 1
+array(1) {
+  [0]=>
+  array(1) {
+    [1]=>
+    int(1)
+  }
 }
+
+*/
+
+echo "<br/>\r\n";
+
+var_dump($db->row('select 1'));
+/*
+This will output:
+
+array(1) {
+  [1]=>
+  int(1)
+}
+
+*/
+
+echo "<br/>\r\n";
+
+var_dump($db->single('select 1'));
+/*
+This will output:
+
+  int(1)
 
 */
